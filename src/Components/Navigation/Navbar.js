1471ec7f9b2f068e7281b36ff4032b02cdbacccd
Marley Anthony <62618323+marleyanthony@users.noreply.github.com>
2022-10-09 16:52:43 -0700

    removed starter code. added navbar

---
+++ src/Components/Navigation/Navbar.js
@@ -0,0 +1,11 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar-wrapper">
+      <h1 className="heading">The Planets</h1>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/Constants/Colors.js
@@ -0,0 +1,3 @@
+export const UI_COLORS = {
+  background: "#070724",
+};
