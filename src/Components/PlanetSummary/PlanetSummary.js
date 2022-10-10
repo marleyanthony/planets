cc29ef6ba3d962f8a7337af38643a2b87908e863
Marley Anthony <62618323+marleyanthony@users.noreply.github.com>
2022-10-09 22:54:58 -0700

    basic app layout

--- src/Components/PlanetSummary/PlanetSummary.js
+++ src/Components/PlanetSummary/PlanetSummary.js
@@ -1,7 +1,35 @@
 import "./PlanetSummary.css";
+import { PlanetSummaryData } from "../../Data/planetSummary";
 
 const PlanetSummary = () => {
-  return <div>PlanetSummary</div>;
+  return PlanetSummaryData.map((planet) => {
+    return (
+      <div className="planet-summary-container">
+        <div className="planet-summary-img-container">
+          <img
+            className="planet-summary-main-img"
+            src={planet.img.default}
+            alt={`${planet.name}`}
+          />
+        </div>
+        <div className="planet-summary-text-container">
+          <h1 className="planet-summary-name">{planet.name}</h1>
+          <p className="planet-summary-overview">{planet.overview}</p>
+          <p className="planet-summary-source">
+            Source:{" "}
+            <a href={planet.overviewWiki} className="planet-summary-link">
+              Wikipedia
+            </a>
+            <img
+              className="planet-summary-link-icon"
+              src={require("../../assets/icon-source.svg").default}
+              alt="icon source"
+            />
+          </p>
+        </div>
+      </div>
+    );
+  });
 };
 
 export default PlanetSummary;
